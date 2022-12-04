69221e1fc3379201aea8c95257c26d072a53daa5
Eduardo Henrique <edu.hen.fm@gmail.com>
2022-12-04 16:25:10 -0400

    Finished room prototype (missing documentation and simulation refactoring)
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- arduino_code/room/room.ino
+++ arduino_code/room/room.ino
@@ -1,22 +1,55 @@
+#include <Servo.h>
+
+Servo curtain;
+int pos = 0;
+
 #define PIR 2
+#define FAN 5
 #define LED 13
 
 int PirSensorState = 0;
+int isCurtainOpen = 0;
+int time = 20000;
 
-void setup() {
+void setup()
+{
   pinMode(PIR, INPUT);
   pinMode(LED, OUTPUT);
-  Serial.begin(9600);
+  pinMode(FAN, OUTPUT);
+  curtain.attach(6);
 }
 
-void loop() {
+void loop()
+{
+  if (millis() >= time) {
+    if (isCurtainOpen) close_curtain();
+    else               open_curtain();
+
+    isCurtainOpen = !isCurtainOpen;
+    time += 20000;
+  }
+
   PirSensorState = digitalRead(PIR);
   
   if (PirSensorState == HIGH) {
     digitalWrite(LED, HIGH);
+    digitalWrite(FAN, HIGH);
   } else {
     digitalWrite(LED, LOW);
+    digitalWrite(FAN, LOW);
   }
-  
-  delay(10);
+}
+
+void open_curtain() {
+  for (pos = 0; pos < 180; pos++) {
+    curtain.write(pos);
+    delay(30);
+  }
+}
+
+void close_curtain() {
+  for (pos = 180; pos > 0; pos--) {
+    curtain.write(pos);
+    delay(30);
+  }
 }
